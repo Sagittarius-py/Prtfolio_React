a946ee4465bdd3e0bc21e28d1dbfe5090a5b7d22
Filip <sieniu212@gmail.com>
2022-07-28 20:59:49 +0200

    Changes to be committed: 	modified:   src/components/section_about.jsx 	modified:   src/style/about_card.css 	modified:   src/style/section_about.css

--- src/components/section_about.jsx
+++ src/components/section_about.jsx
@@ -12,7 +12,22 @@ const About = () => {
 
         <div className="about-container">
           <AboutCard delay="0.5" id="about-card-one">
-            <img src={profile} alt="profile" id="profile-pic" />
+            <div className="pic-container">
+              <img src={profile} alt="profile" id="profile-pic" />
+            </div>
+            <div className="paragraph-container">
+              <p className="about-paragraph">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse vitae nunc commodo, interdum mauris ac, congue elit.
+                Aliquam et turpis et arcu volutpat laoreet. Quisque a vehicula
+                urna, in tincidunt diam. Fusce posuere tortor eu orci tempor
+                aliquam. Curabitur ultrices diam blandit massa vulputate, at
+                semper nisi tristique. Maecenas elementum bibendum mauris, a
+                viverra leo tempus id. Sed quis leo vitae augue iaculis posuere.
+                Maecenas sed massa auctor, malesuada justo vitae, cursus mi. Sed
+                ac urna leo.
+              </p>
+            </div>
           </AboutCard>
           <AboutCard delay="1" id="about-card-two"></AboutCard>
           <AboutCard delay="1.5" id="about-card-three"></AboutCard>
