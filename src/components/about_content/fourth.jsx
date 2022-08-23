96f170b0770d8d82b934aacd719141feb54068b4
Filip <sieniu212@gmail.com>
2022-08-23 21:08:55 +0200

    Changes to be committed: 	modified:   src/components/about_content/fourth.jsx 	modified:   src/style/about_card.css 	modified:   src/style/about_content/fourth.css

--- src/components/about_content/fourth.jsx
+++ src/components/about_content/fourth.jsx
@@ -4,9 +4,30 @@ const Fourth = () => {
   return (
     <>
       <div className="pic-container-4" />
-      <h1 className="title-contact-tile">Contact with me</h1>
+      <h1 className="title-contact-tile">Contact & Socialmedia</h1>
       <h3 className="title-contact-click">Click Here</h3>
-      <div className="test-div"></div>
+      <div className="button-div">
+        <div className="contact-btn-contain" id="phone-button-contain">
+          <button className="contact-button" id="phone-button">
+            Call Me
+          </button>
+        </div>
+        <div className="contact-btn-contain" id="github-button-contain">
+          <button className="contact-button" id="github-button">
+            My Github
+          </button>
+        </div>
+        <div className="contact-btn-contain" id="linked-button-contain">
+          <button className="contact-button" id="linked-button">
+            My linkedIn
+          </button>
+        </div>
+        <div className="contact-btn-contain" id="mail-button-contain">
+          <button className="contact-button" id="mail-button">
+            Write to Me
+          </button>
+        </div>
+      </div>
     </>
   );
 };
